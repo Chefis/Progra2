3487d9bd0b345a0e9193a357220aa7a209e33f8e
marcela leandro <marceleap0425@yahoo.com>
2013-04-16 09:07:06 -0600

    Revert "Revert "Revert "Oscar"""
    This reverts commit 8145b0160dc940cd6b7578fcb1f6fcd059d5b571.

--- src/ManejoInventario/Bodega.java
+++ src/ManejoInventario/Bodega.java
@@ -1,12 +1,9 @@
+
 package ManejoInventario;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Iterator;
-
 /*
- * Creado por: Oscar Mendoza López
- * Fecha: 15/04/2013
+ * Creado por: 
+ * Fecha: 
  * -------------------------------------------------- 
  * Modificado por: 
  * Fecha: 
@@ -15,62 +12,5 @@ import java.util.Iterator;
  * 
  */
 public class Bodega {
-
-    private String nombre = "";
-    private String ubicacion = "";
-    private ArrayList<ProductoInv> lP = new ArrayList<ProductoInv>();
     
-    
-    public Bodega() {
-    }
-
-    public Bodega(String nombre, String ubicacion) {
-        this.nombre = nombre;
-        this.ubicacion = ubicacion;
-    }
-    
-    public ArrayList<ProductoInv> getlP() {
-        return lP;
-    }
-
-    public void setlP(ArrayList<ProductoInv> lP) {
-        this.lP = lP;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getUbicacion() {
-        return ubicacion;
-    }
-
-    public void setUbicacion(String ubicacion) {
-        this.ubicacion = ubicacion;
-    }
-
-    public void agregarProducot(ProductoInv p) {
-        lP.add(p);
-
-    }
-
-    public String verProductos() {
-        String listaP = "";
-        int cont = 0;
-        Iterator<ProductoInv> it = lP.iterator();
-        while (it.hasNext()) {
-            cont++;
-            listaP += cont + "." + it.next().toString() + "\n";
-        }
-        return getNombre() + "Productos:\n" + listaP;
-    }
-    
-    @Override
-    public String toString() {
-        return getNombre() +" "+ getUbicacion();
-    }
 }
--- src/ManejoInventario/Distribuidor.java
+++ src/ManejoInventario/Distribuidor.java
@@ -1,12 +1,8 @@
 
 package ManejoInventario;
-
-import java.util.ArrayList;
-import java.util.Date;
-
 /*
- * Creado por: Oscar Mendoza López 
- * Fecha: 15/04/2013
+ * Creado por: 
+ * Fecha: 
  * -------------------------------------------------- 
  * Modificado por: 
  * Fecha: 
@@ -17,73 +13,5 @@ import java.util.Date;
 
 
 public class Distribuidor {
-    
-    private String nombre = "";
-    private String empresa = "";
-    private String telefonos = "";
-    private Date fechaEntrega;
-   
-    private Categoria tipoProductos;
-    private ArrayList<ProductoInv> lDP = new ArrayList<ProductoInv>();
-
-    public Distribuidor() {
-    }
-
-    public Distribuidor(String nombre, String empresa, String telefonos, Categoria tipoProductos, Date fechaEntrega) {
-        
-        this.nombre = nombre;
-        this.empresa = empresa;
-        this.telefonos = telefonos;
-        this.fechaEntrega = fechaEntrega;
-        this.tipoProductos = tipoProductos;
-    }
-
-     public ArrayList<ProductoInv> getlDP() {
-        return lDP;
-    }
-
-    public void setlDP(ArrayList<ProductoInv> lDP) {
-        this.lDP = lDP;
-    }
-    
-    public Date getFechaEntrega() {
-        return fechaEntrega;
-    }
-
-    public void setFechaEntrega(Date fechaEntrega) {
-        this.fechaEntrega = fechaEntrega;
-    }
-    
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getEmpresa() {
-        return empresa;
-    }
-
-    public void setEmpresa(String empresa) {
-        this.empresa = empresa;
-    }
-
-    public String getTelefonos() {
-        return telefonos;
-    }
-
-    public void setTelefonos(String telefonos) {
-        this.telefonos = telefonos;
-    }
-
-    public Categoria getTipoProductos() {
-        return tipoProductos;
-    }
-
-    public void setTipoProductos(Categoria tipoProductos) {
-        this.tipoProductos = tipoProductos;
-    }
     
 }
