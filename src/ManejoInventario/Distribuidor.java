8145b0160dc940cd6b7578fcb1f6fcd059d5b571
marcela leandro <marceleap0425@yahoo.com>
2013-04-16 09:04:04 -0600

    Revert "Revert "Oscar""
    This reverts commit 3e8d43b0b0fbba0f09a3b98c622a7de8136c90dc.

--- src/ManejoInventario/Distribuidor.java
+++ src/ManejoInventario/Distribuidor.java
@@ -1,8 +1,12 @@
 
 package ManejoInventario;
+
+import java.util.ArrayList;
+import java.util.Date;
+
 /*
- * Creado por: 
- * Fecha: 
+ * Creado por: Oscar Mendoza López 
+ * Fecha: 15/04/2013
  * -------------------------------------------------- 
  * Modificado por: 
  * Fecha: 
@@ -13,5 +17,73 @@ package ManejoInventario;
 
 
 public class Distribuidor {
+    
+    private String nombre = "";
+    private String empresa = "";
+    private String telefonos = "";
+    private Date fechaEntrega;
+   
+    private Categoria tipoProductos;
+    private ArrayList<ProductoInv> lDP = new ArrayList<ProductoInv>();
+
+    public Distribuidor() {
+    }
+
+    public Distribuidor(String nombre, String empresa, String telefonos, Categoria tipoProductos, Date fechaEntrega) {
+        
+        this.nombre = nombre;
+        this.empresa = empresa;
+        this.telefonos = telefonos;
+        this.fechaEntrega = fechaEntrega;
+        this.tipoProductos = tipoProductos;
+    }
+
+     public ArrayList<ProductoInv> getlDP() {
+        return lDP;
+    }
+
+    public void setlDP(ArrayList<ProductoInv> lDP) {
+        this.lDP = lDP;
+    }
+    
+    public Date getFechaEntrega() {
+        return fechaEntrega;
+    }
+
+    public void setFechaEntrega(Date fechaEntrega) {
+        this.fechaEntrega = fechaEntrega;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    public String getTelefonos() {
+        return telefonos;
+    }
+
+    public void setTelefonos(String telefonos) {
+        this.telefonos = telefonos;
+    }
+
+    public Categoria getTipoProductos() {
+        return tipoProductos;
+    }
+
+    public void setTipoProductos(Categoria tipoProductos) {
+        this.tipoProductos = tipoProductos;
+    }
     
 }
