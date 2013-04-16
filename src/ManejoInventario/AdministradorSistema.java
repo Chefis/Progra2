ba857dd68ee061ee737b983a615c1a202c69b254
marcela leandro <marceleap0425@yahoo.com>
2013-04-16 09:21:42 -0600

    Revert "Revert "Revert "Revert "Oscar""""
    This reverts commit 3487d9bd0b345a0e9193a357220aa7a209e33f8e.

--- src/ManejoInventario/AdministradorSistema.java
+++ src/ManejoInventario/AdministradorSistema.java
@@ -4,10 +4,70 @@
  */
 package ManejoInventario;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
  *
  * @author M-01
  */
 public class AdministradorSistema {
     
+
+    private ArrayList<Usuario> lU = new ArrayList<Usuario>();
+    //Esto es para vailidar al adiministrador del sistema
+    private String nombre = "Admin";
+    private String puesto = "Administrador del Sistema";
+    private String contrasena = "1234";
+
+    public AdministradorSistema() {
+    }
+
+    public AdministradorSistema(String nombre, String contrasena) {
+        this.nombre = nombre;
+        this.contrasena = contrasena;
+
+    }
+          
+    public ArrayList<Usuario> getlU() {
+        return lU;
+    }
+
+    public void setlU(ArrayList<Usuario> lU) {
+        this.lU = lU;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public void agregarUsuarios(Usuario u) {
+        lU.add(u);
+
+    }
+
+    public String verUsuarios() {
+        String listaU = "";
+        int cont = 0;
+        Iterator<Usuario> it = lU.iterator();
+        while (it.hasNext()) {
+            cont++;
+            listaU += cont + "." + it.next().toString() + "\n";
+        }
+        return "Usuarios:\n" + listaU;
+    }
+    
 }
+
--- src/ManejoInventario/Bodega.java
+++ src/ManejoInventario/Bodega.java
@@ -1,9 +1,12 @@
-
 package ManejoInventario;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+
 /*
- * Creado por: 
- * Fecha: 
+ * Creado por: Oscar Mendoza López
+ * Fecha: 15/04/2013
  * -------------------------------------------------- 
  * Modificado por: 
  * Fecha: 
@@ -12,5 +15,62 @@ package ManejoInventario;
  * 
  */
 public class Bodega {
+
+    private String nombre = "";
+    private String ubicacion = "";
+    private ArrayList<ProductoInv> lP = new ArrayList<ProductoInv>();
     
+    
+    public Bodega() {
+    }
+
+    public Bodega(String nombre, String ubicacion) {
+        this.nombre = nombre;
+        this.ubicacion = ubicacion;
+    }
+    
+    public ArrayList<ProductoInv> getlP() {
+        return lP;
+    }
+
+    public void setlP(ArrayList<ProductoInv> lP) {
+        this.lP = lP;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public void agregarProducot(ProductoInv p) {
+        lP.add(p);
+
+    }
+
+    public String verProductos() {
+        String listaP = "";
+        int cont = 0;
+        Iterator<ProductoInv> it = lP.iterator();
+        while (it.hasNext()) {
+            cont++;
+            listaP += cont + "." + it.next().toString() + "\n";
+        }
+        return getNombre() + "Productos:\n" + listaP;
+    }
+    
+    @Override
+    public String toString() {
+        return getNombre() +" "+ getUbicacion();
+    }
 }
